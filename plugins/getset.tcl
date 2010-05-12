bf17fb2d4067640806dbb013c65019709b430d8e
emerson <emerson>
2010-05-12 01:29:11 +0000

    ttk

--- plugins/getset.tcl
+++ plugins/getset.tcl
@@ -5,7 +5,7 @@
 
 # tkMOO-light is Copyright (c) Andrew Wilson 1994,1995,1996,1997,1998
 #
-#       All Rights Reserved
+#	   All Rights Reserved
 #
 # Permission is hereby granted to use this software for private, academic
 # and non-commercial use. No commercial or profitable use of this
@@ -41,63 +41,63 @@
 client.register getset start
 
 proc getset.start {} {
-    mcp21.register dns-com-awns-getset 1.0 \
-	dns-com-awns-getset-get getset.do_dns_com_awns_getset_get
-    mcp21.register dns-com-awns-getset 1.0 \
-	dns-com-awns-getset-set getset.do_dns_com_awns_getset_set
-    mcp21.register dns-com-awns-getset 1.0 \
-	dns-com-awns-getset-ack getset.do_dns_com_awns_getset_ack
-    window.menu_tools_add "GetSetDrop" getset_app.create
+	mcp21.register dns-com-awns-getset 1.0 \
+			dns-com-awns-getset-get getset.do_dns_com_awns_getset_get
+	mcp21.register dns-com-awns-getset 1.0 \
+			dns-com-awns-getset-set getset.do_dns_com_awns_getset_set
+	mcp21.register dns-com-awns-getset 1.0 \
+			dns-com-awns-getset-ack getset.do_dns_com_awns_getset_ack
+	window.menu_tools_add "GetSetDrop" getset_app.create
 }
 
 proc getset.do_dns_com_awns_getset_get {} {
-    # client-side this is a no-op
+	# client-side this is a no-op
 }
 
 proc getset.do_dns_com_awns_getset_set {} {
-    # client-side this is a no-op
+	# client-side this is a no-op
 }
 
 proc getset.do_dns_com_awns_getset_ack {} {
-    global getset_db
-    set id [request.get current id]
-    if { [info exists getset_db($id,callback)] } {
-	$getset_db($id,callback)
-	unset getset_db($id,callback)
-    }
+	global getset_db
+	set id [request.get current id]
+	if { [info exists getset_db($id,callback)] } {
+		$getset_db($id,callback)
+		unset getset_db($id,callback)
+	}
 }
 
 # .get => {}
-#         {value}
+#		 {value}
 proc getset.get {property {callback ""}} {
-    global getset_db
-    set id [util.unique_id getset]
-    if { $callback != "" } {
-        set getset_db($id,callback) $callback
-    }
-    mcp21.server_notify dns-com-awns-getset-get \
+	global getset_db
+	set id [util.unique_id getset]
+	if { $callback != "" } {
+		set getset_db($id,callback) $callback
+	}
+	mcp21.server_notify dns-com-awns-getset-get \
 	[list [list id "$id"] [list property "$property"]]
 }
 
 # .set => {}
 proc getset.set {property value {callback ""}} {
-    global getset_db
-    set id [util.unique_id getset]
-    if { $callback != "" } {
-        set getset_db($id,callback) $callback
-    }
-    mcp21.server_notify dns-com-awns-getset-set \
+	global getset_db
+	set id [util.unique_id getset]
+	if { $callback != "" } {
+		set getset_db($id,callback) $callback
+	}
+	mcp21.server_notify dns-com-awns-getset-set \
 	[list [list id "$id"] [list property "$property"] [list value "$value"]]
 }
 
 # .drop => {}
 proc getset.drop {property {callback ""}} {
-    global getset_db
-    set id [util.unique_id getset]
-    if { $callback != "" } {
-        set getset_db($id,callback) $callback
-    }
-    mcp21.server_notify dns-com-awns-getset-drop \
+	global getset_db
+	set id [util.unique_id getset]
+	if { $callback != "" } {
+		set getset_db($id,callback) $callback
+	}
+	mcp21.server_notify dns-com-awns-getset-drop \
 	[list [list id "$id"] [list property "$property"]]
 }
 
@@ -106,58 +106,58 @@ proc getset.drop {property {callback ""}} {
 # a text widget and buttons for [get] [set] [drop]
 
 proc getset_app.create {} {
-    set w .getsetapp
-    catch { destroy .getsetapp }
-    toplevel $w
-    text $w.t -width 30 -height 10
-    frame $w.f
-    button $w.f.get -width 4 -text get -command getset_app.do_get
-    button $w.f.set -width 4 -text set -command getset_app.do_set
-    button $w.f.drop -width 4 -text drop -command getset_app.do_drop
-    pack $w.t -side top
-    pack $w.f -side bottom
-    pack $w.f.get -side left
-    pack $w.f.set -side left
-    pack $w.f.drop -side left
+	set w .getsetapp
+	catch { destroy .getsetapp }
+	toplevel $w
+	text $w.t -width 30 -height 10
+	ttk::frame $w.f
+	ttk::button $w.f.get -width 4 -text get -command getset_app.do_get
+	ttk::button $w.f.set -width 4 -text set -command getset_app.do_set
+	ttk::button $w.f.drop -width 4 -text drop -command getset_app.do_drop
+	pack $w.t -side top
+	pack $w.f -side bottom
+	pack $w.f.get -side left
+	pack $w.f.set -side left
+	pack $w.f.drop -side left
 }
 
 proc getset_app.do_get {} {
-    getset.get getset_app.text getset_app.do_get_ack
+	getset.get getset_app.text getset_app.do_get_ack
 }
 
 proc getset_app.do_set {} {
-    set value [getset_app.get_text .getsetapp.t]
-    getset.set getset_app.text "$value" getset_app.do_ack
+	set value [getset_app.get_text .getsetapp.t]
+	getset.set getset_app.text "$value" getset_app.do_ack
 }
 
 proc getset_app.do_ack {} {
-    window.displayCR "getset_app.do_ack"
+	window.displayCR "getset_app.do_ack"
 }
 
 proc getset_app.do_get_ack {} {
-    set value [request.get current value]
-    .getsetapp.t delete 1.0 end
-    getset_app.set_text .getsetapp.t $value
+	set value [request.get current value]
+	.getsetapp.t delete 1.0 end
+	getset_app.set_text .getsetapp.t $value
 }
 
 proc getset_app.do_drop {} {
-    getset.drop getset_app.text getset_app.do_ack
+	getset.drop getset_app.text getset_app.do_ack
 }
 
 proc getset_app.get_text t {
-    set lines {}
-    set last [$t index end]
-    for {set n 1} {$n < $last} {incr n} {
-        set line [$t get "$n.0" "$n.0 lineend"] 
-        lappend lines $line
-    }
-    return $lines
+	set lines {}
+	set last [$t index end]
+	for {set n 1} {$n < $last} {incr n} {
+		set line [$t get "$n.0" "$n.0 lineend"] 
+		lappend lines $line
+	}
+	return $lines
 }   
 
 proc getset_app.set_text { t lines } {
-    set CR ""
-    foreach line $lines {
-        $t insert end "$CR$line"
-        set CR "\n"
-    }
+	set CR ""
+	foreach line $lines {
+		$t insert end "$CR$line"
+		set CR "\n"
+	}
 }
